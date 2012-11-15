c7b0c6b7c7f60d10a072416bc33897c69b3f2ec0
robliuning <robliuning@gmail.com>
2012-11-15 18:15:32 +0200

    T1S1:Project initilizing

---
+++ src/com/example/geotrack/FragmentAbout.java
@@ -0,0 +1,5 @@
+package com.example.geotrack;
+
+public class FragmentAbout {
+
+}
---
+++ src/com/example/geotrack/TrackActivity.java
@@ -0,0 +1,35 @@
+package com.example.geotrack;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class TrackActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_track);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_track, menu);
+        return true;
+    }
+
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
